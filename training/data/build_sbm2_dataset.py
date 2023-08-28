46e1858e5024220eafb7f5cd32e88f38949cbc0e
sofiperez91 <sperezcasulo@gmail.com>
2023-08-28 19:21:29 -0300

    uploading dataset builders

---
+++ training/data/build_sbm2_dataset.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append("../")
+
+import torch
+import pickle
+from torch_geometric.utils import stochastic_blockmodel_graph
+from torch_geometric.data import Data
+
+num_nodes = 100
+d = 2
+device = 'cpu'
+TRAIN_DATA_FILE='./sbm2_train.pkl'
+VAL_DATA_FILE='./sbm2_val.pkl'
+
+n = [int(num_nodes/2), int(num_nodes/2)]
+
+p = [
+     [0.9, 0.1],
+     [0.1, 0.5]
+]
+
+df_train = []
+df_val = []
+
+for j in range(1000):
+    x = torch.rand((num_nodes, d))
+    edge_index = stochastic_blockmodel_graph(n, p).to(device)
+    data = Data(x = x, edge_index = edge_index)
+    if j < 800:
+        df_train.append(data)
+    else:
+        df_val.append(data)
+
+with open(TRAIN_DATA_FILE, 'wb') as f:
+    pickle.dump(df_train, f)
+with open(VAL_DATA_FILE, 'wb') as f:
+    pickle.dump(df_val, f)
---
+++ training/data/build_sbm3_dataset.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("../")
+
+import torch
+import pickle
+from torch_geometric.utils import stochastic_blockmodel_graph
+from torch_geometric.data import Data
+
+num_nodes = 150
+d = 3
+device = 'cpu'
+TRAIN_DATA_FILE='./data/sbm3_train.pkl'
+VAL_DATA_FILE='./data/sbm3_val.pkl'
+
+n = [int(num_nodes/3), int(num_nodes/3), int(num_nodes/3)]
+
+p = [
+     [0.5, 0.1, 0.3],
+     [0.1, 0.9, 0.2], 
+     [0.3, 0.2, 0.7]
+]
+
+df_train = []
+df_val = []
+
+for j in range(1000):
+    x = torch.rand((num_nodes, d))
+    edge_index = stochastic_blockmodel_graph(n, p).to(device)
+    data = Data(x = x, edge_index = edge_index)
+    if j < 800:
+        df_train.append(data)
+    else:
+        df_val.append(data)
+
+with open(TRAIN_DATA_FILE, 'wb') as f:
+    pickle.dump(df_train, f)
+with open(VAL_DATA_FILE, 'wb') as f:
+    pickle.dump(df_val, f)
